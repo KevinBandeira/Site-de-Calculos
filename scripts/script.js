d99487de3e19697a6b04d0b806b90803bd9dae35
Kevin <kevinbandeira2006@gmail.com>
2024-06-07 19:20:35 -0300

    Criando a navigation responsiva, com possivel mudança de esterelização.

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+function menu() {
+    if (itens.style.display == 'none') {
+        itens.style.display = 'block'
+        men.style.marginTop = '68.52%'
+        image.style.display = 'block'
+        img.style.display = 'none'
+    }
+    else {
+        itens.style.display = 'none'
+        men.style.marginTop = '0%'
+        image.style.display = 'none'
+        img.style.display = 'block'
+    }
+}
+
+function calc() {
+    
+}
